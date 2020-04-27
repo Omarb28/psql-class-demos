c66354cc3191e7cd6dc822e5bd2e01de4b7d7fbf
Omarb28 <omarb28@gmail.com>
2020-04-27 01:11:54 +0000

    fixed typo

--- run_flask.bat
+++ run_flask.bat
@@ -1,2 +1,2 @@
 export FLASK_ENV=development
-FLASK_APP=flask-hello-app.py FLASK_DEBUG=true flask run --host=0.0.0.0 --port=8000
+FLASK_APP=flask_hello_app.py FLASK_DEBUG=true flask run --host=0.0.0.0 --port=8000
